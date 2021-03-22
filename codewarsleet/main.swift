2b30ec4ffe8fa287d0e98d425ca15b1eb65676d7
Aidar <chakiev_i@garpix.com>
2021-03-22 12:49:24 +0600

    initial

--- codewarsleet/main.swift
+++ codewarsleet/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+let problem = Solution()
+print(problem.findDeletedNumber([1,2,3,4,5,6,7,8,9],[3,2,4,6,7,8,1,9]))
 
+class Solution{
+    func findDeletedNumber(_ array: [Int], _ mixArray: [Int]) -> Int {
+        var newArray = mixArray
+        var res = 0
+        newArray.sort()
+        for i in newArray.indices {
+            if newArray[i] != array[i] {
+                res = array[i]
+                break
+            }
+        }
+        return res
+    }
+}
